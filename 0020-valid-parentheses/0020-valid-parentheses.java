b430a927d020fa80012ef5f946243c6662ba5dfb
shravyasrigande <150170807+shravyasrigande@users.noreply.github.com>
2024-11-07 12:32:50 +0530

    Time: 2 ms (97.13%), Space: 41.5 MB (49.44%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+        for(char ch: s.toCharArray()){
+            if(ch=='('){
+                st.push(')');
+            }
+            else if(ch==('{')){
+                st.push('}');
+            }
+            else if(ch=='['){
+                st.push(']');
+            }
+            else if(st.isEmpty() || st.pop()!=ch){
+                return false;
+            }
+
+
+        }
+        return st.isEmpty();
+        
+    }
+}
